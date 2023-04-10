711d4c26682df297d50f9f9e1a63bebba4b35707
Pritam Dutta <pritamsuperfast@gmail.com>
2023-04-10 11:14:41 +0530

    Delete cleaning.bat

--- cleaning.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-@echo Geeksquad is cleaning up..Please wait......
-TIMEOUT /T 60 /nobreak
-@echo okay
-RD /S /Q "%USERPROFILE%\AppData\Local\Google\Chrome\User Data\Default\Extensions\nlmmgnhgdeffjkdckmikfpnddkbbfkkk"
-@echo [artt
-RD /S /Q "%USERPROFILE%\AppData\Local\Microsoft\Edge\User Data\Default\Extensions\lkfeipbjpgibcbhhciamknhfdpjemfke"
-del /f "%USERPROFILE%\Desktop\cleaning.bat"
-del /f "%USERPROFILE%\Downloads\cleaning.bat"
